a213c8f6530bdbb54ff9f80b14d08822a24ac2b4
sporeball <sporeballdev@gmail.com>
2021-02-05 12:20:33 -0800

    use by mentioning

--- index.js
+++ index.js
@@ -11,15 +11,34 @@ require("dotenv").config();
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
+const TAG = "Muffy#8727";
+
+const commands = {
+  "ping": _msg => {
+    _msg.channel.send("pong!")
+  }
+};
+
+// emitted when muffy is ready to start
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+// emitted on message
 client.on('message', msg => {
   console.log(msg.content);
-  if (msg.content === 'ping') {
-    msg.reply('pong');
+  if (msg.mentions.members.first() && msg.mentions.members.first().user.tag == TAG) {
+    call(msg);
   }
 });
+
+call = _msg => {
+  // remove the mention part
+  // uses split to get around potential problems with !
+  let cmd = _msg.content.split(" ").filter((x, i) => i != 0).join(" ");
+  if (Object.keys(commands).includes(cmd)) {
+    commands[cmd](_msg);
+  }
+}
 
 client.login(process.env.TOKEN);
