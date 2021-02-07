624ec7152109dbbeeb6507121a122c9b3914c75b
sporeball <sporeballdev@gmail.com>
2021-02-06 22:11:48 -0800

    first db test

--- index.js
+++ index.js
@@ -10,12 +10,31 @@ require("dotenv").config();
 
 const Discord = require("discord.js");
 const client = new Discord.Client();
-
 const TAG = "Muffy#8727";
 
+const Conf = require("conf");
+const conf = new Conf({
+  configName: "db",
+  cwd: "."
+});
+
 const commands = {
-  "ping": _msg => {
+  "ping": (args, _msg) => {
     _msg.channel.send("pong!")
+  },
+  "whitelist": (args, _msg) => {
+    let whitelist = `users.${_msg.author.id}.${_msg.guild.id}.whitelist`;
+    if (args.length == 0) {
+      _msg.channel.send(`your whitelist is ${(conf.get(whitelist) === undefined || conf.get(whitelist).length == 0) ? "empty!" : conf.get(whitelist).map(x => `<#${x}>`).join(", ")}`);
+    } else {
+      if (args[0] == "reset") {
+        conf.set(whitelist, []);
+        _msg.channel.send("reset your whitelist!");
+      } else {
+        conf.set(whitelist, _msg.mentions.channels.array().map(x => x.id));
+        _msg.channel.send(`updated your whitelist!`);
+      }
+    }
   }
 };
 
@@ -35,9 +54,12 @@ client.on('message', msg => {
 call = _msg => {
   // remove the mention part
   // uses split to get around potential problems with !
-  let cmd = _msg.content.split(" ").filter((x, i) => i != 0).join(" ");
+  let m = _msg.content.split(" ").filter((x, i) => i != 0);
+  let cmd = m[0];
+  let args = m.filter((x, i) => i != 0);
+
   if (Object.keys(commands).includes(cmd)) {
-    commands[cmd](_msg);
+    commands[cmd](args, _msg);
   }
 }
 
